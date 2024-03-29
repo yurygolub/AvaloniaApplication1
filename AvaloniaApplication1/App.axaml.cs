3a5328ef000260b77c0a637b70a0357d1fccc909
Yury Golub <yuragolub21@gmail.com>
2024-03-29 20:40:27 +0300

    Add analyzers. Fix issues.

--- AvaloniaApplication1/App.axaml.cs
+++ AvaloniaApplication1/App.axaml.cs
@@ -22,14 +22,14 @@ public partial class App : Application
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainViewModel()
+                DataContext = new MainViewModel(),
             };
         }
         else if (this.ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
             singleViewPlatform.MainView = new MainView
             {
-                DataContext = new MainViewModel()
+                DataContext = new MainViewModel(),
             };
         }
 
